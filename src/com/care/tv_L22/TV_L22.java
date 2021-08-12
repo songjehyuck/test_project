cd35c7bdf233cc4a71fc8714034808cc0713f31c
Hoon <dkhj0615@naver.com>
2021-08-12 11:34:35 +0900

    merge후 재 업로드

--- src/com/care/tv_L22/TV_L22.java
+++ src/com/care/tv_L22/TV_L22.java
@@ -1,5 +1,22 @@
 package com.care.tv_L22;
 
-public class TV_L22 {
+import java.util.Scanner;
 
+public class TV_L22 {
+	public void display() {
+		System.out.println("냉장고 하는 기능입니다.");
+		Scanner input = new Scanner(System.in);
+		int num;
+		System.out.println("1.올림, 2.내림");
+		num=input.nextInt();
+		if(num==1)tempUp();
+		else tempDown();
+	}
+	private void tempUp() {
+		System.out.println("온도를 올립니다.");
+	}
+	private void tempDown() {
+		System.out.println("온도를 낮춥니다.");
+	}
 }
+
