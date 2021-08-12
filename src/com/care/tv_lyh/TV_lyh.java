d0c5b4c1166d3569ba638840f4225ef3f30448c1
dydgns2446 <dydgns2446@naver.com>
2021-08-12 11:20:50 +0900

    tv코드 작성

---
+++ src/com/care/tv_lyh/TV_lyh.java
@@ -0,0 +1,21 @@
+package com.care.tv_lyh;
+
+import java.util.Scanner;
+
+public class TV_lyh {
+	public void display() {
+		System.out.println("냉장고 하는 기능입니다");
+		Scanner in = new Scanner(System.in);
+		int num;
+		System.out.println("1.올림, 2.내림");
+		num = in.nextInt();
+		if(num==1) tempUp();
+		else tempDown();
+	}
+	private void tempUp() {
+		System.out.println("온도를 올립니다");
+	}
+	private void tempDown() {
+		System.out.println("온도를 내립니다");
+	}
+}
