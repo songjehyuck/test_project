b5013a2da8a1d03ba7f1560048f404ba00917f71
jehyuck <zzukk78@naver.com>
2021-08-12 11:20:09 +0900

    tv 코드 작성

--- src/com/care/def/MainClass.java
+++ src/com/care/def/MainClass.java
@@ -1,5 +1,29 @@
 package com.care.def;
 
-public class MainClass {
+import java.util.Scanner;
 
+public class MainClass {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		int num;
+		System.out.println("---∞°¿¸¡¶«∞---");
+		while(true) {
+			System.out.println("1.≥√¿Â∞Ì");
+			System.out.println("2.Tv");
+			System.out.println("3.ºº≈π±‚");
+			System.out.print(">>>> : ");
+			num=input.nextInt();
+			switch(num) {
+			case 1:
+				System.out.println("≥√¿Â∞Ì");
+				break;
+			case 2:
+				System.out.println("Tv");
+				break;
+			case 3:
+				System.out.println("ºº≈π±‚");
+				break;
+			}
+		}
+	}
 }
